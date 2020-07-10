f2b86e4aa798daa4508ef4db07bcf7141b253a67
Fernando Hernandez <Fernando.Hernandez.Fabars@gmail.com>
2020-07-10 12:38:59 -0500

    the missing java files

---
+++ src/ServerName.java
@@ -0,0 +1,15 @@
+public class ServerName {
+        String[] adj = {"disgusting", "juicy", "soggy", "thick", "romantic", "clumsy", "foolish", "shiny", "doubtful" ,"overrated"};
+        String[] nouns = {"internet", "oven", "computer", "surgery", "entry", "king", "attitude", "death", "wedding", "song"};
+
+        public static String randomName(String[] adj, String[] nouns){
+            int a = (int) (Math.floor(Math.random() * adj.length));
+            int n = (int) (Math.floor(Math.random() * nouns.length));
+            return adj[a] + " " + nouns[n];
+        }
+
+        public static void main(String[] args) {
+            ServerName nameGenerator = new ServerName();
+            System.out.println("Here is your Server name: \n" + randomName(nameGenerator.adj, nameGenerator.nouns));
+        }
+    }
